9a17cbf817acc0d27e55942b1bc98377077a2826
Eric Smith <ydrive65@yahoo.com>
2022-02-22 16:53:46 -0500

    move config generator to own file

---
+++ SG5.SourceGenerators/ConfigGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace SG5.SourceGenerators
+{
+    [Generator]
+    public class ConfigGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) { }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            bool isEnabled = false;
+            if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("builder_property.MyGenerator_IsEnabled", out var amIActive))
+            {
+                isEnabled = amIActive.Equals("true", StringComparison.OrdinalIgnoreCase);
+            }
+
+            var mySwitch = $@"
+namespace Generated;
+public static class MySwitch
+{{
+    public const string Test = ""I was {(isEnabled ? "enabled" : "disabled")} at compile time."";
+}}
+";
+
+            context.AddSource("Enabled.g.cs", mySwitch);
+        }
+    }
+}
--- SG5.SourceGenerators/FromFileGenerator.cs
+++ SG5.SourceGenerators/FromFileGenerator.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -35,31 +34,6 @@ public static class MyStrings
 }}
 ";
             context.AddSource($"MyStrings.generated.cs", myStrings);
-        }
-    }
-
-    [Generator]
-    public class ConfigGenerator : ISourceGenerator
-    {
-        public void Initialize(GeneratorInitializationContext context) { }
-
-        public void Execute(GeneratorExecutionContext context)
-        {
-            bool isEnabled = false;
-            if (context.AnalyzerConfigOptions.GlobalOptions.TryGetValue("is_enabled", out var amIActive))
-            {
-                isEnabled = amIActive.Equals("true", StringComparison.OrdinalIgnoreCase);
-            }
-
-            var mySwitch = $@"
-namespace Generated;
-public static class MySwitch
-{{
-    public const string Test = ""I was {(isEnabled ? "enabled" : "disabled")} at compile time."";
-}}
-";
-
-            context.AddSource("Enabled.g.cs", mySwitch);
         }
     }
 }
